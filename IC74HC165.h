b8242c0d54d6c774f27afdee8d7f867fb1b91dbe
preeve9534 <preeve@pdjr.eu>
2022-12-14 12:16:02 +0000

    Update IC74HC165.h

--- IC74HC165.h
+++ IC74HC165.h
@@ -1,32 +1,6 @@
 /**********************************************************************
  * IC74HC165.h - ADT for accessing a 74HC165 PISO buffer.
  * 2022 (c) Paul Reeve <preeve@pdjr.eu>
- * 
- * * 
- * The 74HC165 is an 8-bit parallel-to-serial I/O buffer. This library
- * allows the host application to read the buffer.
- * 
- * Example:
- * 
- * #define PisoDataGpio 2
- * #define PisoLatchGpio 3
- * #define PisoClockGpio 4
- * #define MyInterestingBit 6
- * 
- * IC74HC165 piso = IC74HC165(PisoDataGpio, PisoLatchGpio, PisoClockGpio);
- * 
- * void setup() {
- *   piso.begin();
- * }
- * 
- * void loop() {
- *   uint8_t byte;
- *   int bit;
- *
- *   byte = piso.readByte();
- *   bit = piso.readBit(MyInterestingBit); 
- * }
- * 
  */
 
 #ifndef IC74HC165_H
