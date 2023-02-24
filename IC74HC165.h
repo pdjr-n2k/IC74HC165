c3aa02e3cf014f9eeaefe73f5e729a4e16a22596
Paul Reeve <preeve@pdjr.eu>
2023-02-24 11:01:23 +0100

    Update IC74HC165.h

--- IC74HC165.h
+++ IC74HC165.h
@@ -1,7 +1,7 @@
 /**********************************************************************
  * @file IC74HC165.h
  * @author Paul Reeve (preeve@pdjr.eu)
- * @brief Interface for the 74HC165 PISO buffer.
+ * @brief Interface for a 74HC165-based PISO buffer.
  * @version 0.1
  * @date 2023-02-06
  * @copyright Copyright (c) 2023
@@ -14,7 +14,8 @@
  * @brief Interface for a 74HC165-based PISO buffer.
  * 
  * A buffer consists of one or more daisy-chained IC74HC165 ICs each of
- * which maintains an 8-bit status.
+ * which maintains an 8-bit status byte which reflects the state of its
+ * parallel inputs.
  * 
  * This library allows the hardware interface to a buffer to be
  * configured and initialised and allows the buffer to be interrogated
@@ -30,7 +31,8 @@ class IC74HC165 {
      * @param gpioClock - the GPIO pin connected to IC pin 2 (CP/clock).
      * @param gpioData - the GPIO pin connected to IC pin 9 (Q7/data).
      * @param gpioLatch - the GPIO pin connected to IC pin 1 (PL/latch).
-     * @param bufferCount - the number of buffer ICs in the daisy-chain.
+     * @param bufferCount - the number of buffer ICs in the daisy-chain
+     * (defaults to one).
      */
     IC74HC165(uint8_t gpioClock, uint8_t gpioData, uint8_t gpioLatch, unsigned int bufferCount = 1);
 
@@ -38,7 +40,7 @@ class IC74HC165 {
      * @brief Initialise the connection to the PISO buffer.
      * 
      * Set the I/O mode of the GPIO pins used to interface with the
-     * buffer. Must be called from setup().
+     * buffer. Best called from setup().
      */
      void begin();
 
@@ -57,8 +59,8 @@ class IC74HC165 {
     /******************************************************************
      * @brief Get the value of a specified bit in the PISO buffer.
      * 
-     * @param bit - the bit to be selected.
-     * @return uint8_t - the state of the specified bit (0 or 1).
+     * @param bit - the selected bit.
+     * @return uint8_t - the state of the selected bit (0 or 1).
      */
     uint8_t readBit(unsigned int bit);
 
@@ -72,9 +74,9 @@ class IC74HC165 {
      * Make a call to configureCallback() in setup() and make a call to
      * callbackMaybe() in loop().
      * 
-     * @param callback - this function will be called with the status
-     * of each IC in the buffer passed as an element in the array
-     * passed as its sole argument.
+     * @param callback - this function will be called with an array
+     * containing the status bytes of each IC in the buffer
+     * daisy-chain.
      * @param callbackInterval - the interval in milliseconds between
      * successive invocations of the callback function (defaults to
      * 1s).
@@ -82,9 +84,9 @@ class IC74HC165 {
     void configureCallback(void (*callback)(uint8_t *), unsigned long callbackInterval = 1000UL);
     
     /**
-     * @brief Maybe invoke a configured callback.
+     * @brief Maybe invoke the configured callback.
      * 
-     * This method should be called from loop() and if force is false
+     * This method should be called from loop() and, if force is false,
      * will determine when to invoke the callback function based upon
      * the specified update interval.
      * 
